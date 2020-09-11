3ffb8921d9e1100001401e8605f19ac8c6a94a51
Aya <Aya@123.gov>
2020-09-11 09:59:40 -0700

    add all

---
+++ lib/cc/Load.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'Map.dart';
+import 'dart:async';
+
+
+
+
+
+class Load extends StatefulWidget {
+  @override
+  _LoadState createState() => _LoadState();
+}
+
+class _LoadState extends State<Load> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.push(context,MaterialPageRoute(builder: (context)=>Map(),));
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Container(
+        //Background Color
+        decoration: BoxDecoration(
+        gradient: LinearGradient(
+        colors:[
+        Color(0xFF4840B1),
+        Color(0xFFb44BC4),
+    ],
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+    ),
+    ),
+     child:Container(
+      padding: EdgeInsets.only(top:9),
+     margin: EdgeInsets.only(top: 11),
+     child:Image.asset('lib/assets/load.png',
+     //height: 690,
+       //width: 690,
+       //fit: BoxFit.cover,
+
+     ),
+    ),
+      ),
+    );
+  }
+
+  }
+
+
