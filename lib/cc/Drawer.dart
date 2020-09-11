89d304a0e7f7a47ba8c208bda7073cf8db72f897
Aya <Aya@123.gov>
2020-09-11 09:10:25 -0700

    Drawer

---
+++ lib/cc/Drawer.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+
+
+
+        child: ListView(
+
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors:[
+                    Color(0xFF4840B1),
+                    Color(0xFFb44BC4),
+                  ],
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                ),
+              ),
+              
+              child: Stack(
+                children: <Widget>[
+
+                  Align(
+                    alignment: Alignment(-0.7,0.6),
+                    child: CircleAvatar(
+                      //child: Image.asset('lib/assets/boy.jpg',),
+                     backgroundColor: Colors.white70,
+                      radius:52,
+                    ),
+
+                  ),
+
+                  Image.asset('lib/assets/head.png',),
+                  Align(
+                    alignment: Alignment.centerRight+Alignment(-0.6, 0),
+                    child: Text("Ahmad",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    ),
+                  ),
+
+
+
+                  Align(
+                    alignment: Alignment.centerRight+Alignment(0, 0.4),
+                    child: Text("Ahmad123@gmail.com",
+                      style: TextStyle(
+                        color: Colors.white70,
+                       // fontSize: 20.0,
+                      ),
+                    ),
+                  )
+
+
+
+
+                ],
+              ),
+
+              
+            ),
+
+            ListTile(
+              //title: Text('الصفحة الرئيسية'),
+             title: Text('الصفحة الرئيسية',textAlign: TextAlign.right,),
+              leading:Align(
+                alignment: Alignment(1, 2),
+                child :Icon(Icons.home,),),
+                 
+            )
+          ],
+          
+        ),
+        
+      
+      
+      
+      
+      
+     
+    );
+  }
+}
--- lib/cc/Map.dart
+++ lib/cc/Map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'Drawer.dart';
 
 
 
@@ -14,9 +14,49 @@ class _MapState extends State<Map> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Space Map'),
-        backgroundColor: Color(0xFF4840B1),
+        title: Text('Space Map',
+       //textAlign: TextAlign.center,
       ),
+
+
+        centerTitle: true,
+      leading:Directionality(textDirection: TextDirection.ltr, child:
+        Container(
+
+
+
+          padding: EdgeInsets.only(left: 0,top: 2,),
+          margin: EdgeInsets.only(left: 0,),
+
+          //color: Colors.white,
+          child:Stack(
+          children: <Widget>[
+          Align(
+            alignment: Alignment(2, 0),
+           child: IconButton
+              (icon: Icon(Icons.flag,
+              color: Color(0xFFF99E2B),
+              size:35,
+            ),
+            ),),
+            Align(
+              alignment: Alignment(-0.5, 0.5),
+              child: Text("0x",),
+            ),
+
+          ],
+
+          ),
+
+        ),
+      ),
+
+
+
+        backgroundColor: Color(0xFF4840B1),
+        elevation: 6,
+      ),
+      endDrawer: MyDrawer(),
       body: Container(
         //Background Color
         decoration: BoxDecoration(
@@ -30,6 +70,7 @@ class _MapState extends State<Map> {
           ),
         ),
       ),
+
     );
   }
 
